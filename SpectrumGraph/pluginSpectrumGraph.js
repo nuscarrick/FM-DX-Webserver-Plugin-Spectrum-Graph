4aaf201984f51dc23ccc0e29843766c3e8887768
nuscarrick <carrick@nustechnology.com>
2026-01-08 13:14:09 +0700

    Fix missing icon text for spectrum plugin

--- SpectrumGraph/pluginSpectrumGraph.js
+++ SpectrumGraph/pluginSpectrumGraph.js
@@ -361,7 +361,7 @@ function createButton(buttonId) {
         const observer = new MutationObserver((mutationsList, observer) => {
             if (typeof addIconToPluginPanel === 'function') {
                 observer.disconnect();
-                addIconToPluginPanel(buttonId, t('plugin.spectrum'), "solid", "chart-area", t('plugin.spectrumPlugin.showAllSpectrum'));
+                addIconToPluginPanel(buttonId, t('plugin.spectrum'), "solid", "chart-area", t('plugin.showAllSpectrum'));
                 functionFound = true;
 
                 // Setup early click handler to queue clicks during initialisation
